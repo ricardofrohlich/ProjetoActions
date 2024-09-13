f82a0ac3d80a88d46164c8b0a8f920bc5f36acb4
ricardofrolhich <ricardosma@gmail.com>
2024-09-13 09:43:09 -0300

    Primeiro commit: serviço Spring Boot simples Olá Mundo

---
+++ src/main/java/info/frohlich/projetoactions/ProjetoActionsApplication.java
@@ -0,0 +1,13 @@
+package info.frohlich.projetoactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoActionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetoActionsApplication.class, args);
+    }
+
+}
